e514a12fb9caeb8e2d0e9ac079a667c7da9e1e66
Sandwichie123 <fahadsiddiqi6810@gmail.com>
2025-11-19 19:12:53 -0500

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class Constants {
+    // CAN IDs for the drive motors
+    public static final int kfrontDriverDriveID = 2;
+    public static final int kfrontPassengerDriveID = 4;
+    public static final int kbackDriverDriveID = 6;
+    public static final int kbackPassengerDriveID = 8;
+
+    // CAN IDs for the swerve motors
+    public static final int kfrontDriverSwerveID = 1;
+    public static final int kfrontPassengerSwerveID = 1;
+    public static final int kbackDriverSwerveID = 1;
+    public static final int kbackPassengerSwerveID = 1;
+}
