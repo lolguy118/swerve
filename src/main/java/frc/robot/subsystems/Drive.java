16b68bb72bbbef0d4926cf47faa51d922b35029f
lolguy118 <146142050+lolguy118@users.noreply.github.com>
2025-12-15 01:45:33 +0000

    worked on Drive classes

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -1,19 +1,9 @@
 package frc.robot.subsystems;
-
-import com.ctre.phoenix6.hardware.TalonFX;
-
-import frc.robot.Constants;
+import frc.robot.subsystems.SwerveModule.SwerveModuleLocation;
 
 public class Drive {
-    // instantiage all drive motors
-    private final TalonFX kfrontDriverDrive = new TalonFX(Constants.kfrontDriverDriveID, Constants.kCANBusName);
-    private final TalonFX kfrontPassengerDrive = new TalonFX(Constants.kfrontPassengerDriveID, Constants.kCANBusName);
-    private final TalonFX kbackDriverDrive = new TalonFX(Constants.kbackDriverDriveID, Constants.kCANBusName);
-    private final TalonFX kbackPassengerDrive = new TalonFX(Constants.kbackPassengerDriveID, Constants.kCANBusName);
-
-    // instantiate all swerve motors
-    private final TalonFX kfrontDriverSwerve = new TalonFX(Constants.kfrontDriverSwerveID, Constants.kCANBusName);
-    private final TalonFX kfrontPassengerSwerve = new TalonFX(Constants.kfrontPassengerSwerveID, Constants.kCANBusName);
-    private final TalonFX kbackDriverSwerve = new TalonFX(Constants.kbackDriverSwerveID, Constants.kCANBusName);
-    private final TalonFX kbackPassengerSwerve = new TalonFX(Constants.kbackPassengerSwerveID, Constants.kCANBusName);
+    private final SwerveModule frontDriver = new SwerveModule(SwerveModuleLocation.FRONT_DRIVER);
+    private final SwerveModule frontPassenger = new SwerveModule(SwerveModuleLocation.FRONT_PASSENGER);
+    private final SwerveModule backDriver = new SwerveModule(SwerveModuleLocation.BACK_DRIVER);
+    private final SwerveModule backPassenger = new SwerveModule(SwerveModuleLocation.BACK_PASSENGER);
 }
